a3a4b432c8da35a64aded79a6a08740e163b15a6
fadilnatakusumah <fadil.ntksmh@gmail.com>
2019-02-01 18:54:42 +0700

    adding resume file

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -31,7 +31,7 @@ export class Resume extends Component {
 
             <div>
 
-              <a href={'#'} style={{textAlign: 'center'}}>
+              <a href={'https://doc-04-9k-docs.googleusercontent.com/docs/securesc/medpg10mmku73a8sfa12h5frp57f6fs7/dvkl6dl64dilrc8lvi5ck8gamnv2nld3/1549015200000/00994631078964713686/00994631078964713686/1W1b3bWstx90XUO7eRugJoTDyf21HAdJV?e=download&nonce=qc7tq781k4li8&user=00994631078964713686&hash=7mk0tti8jcpkhuek7unje2aoqqcis1bh'} style={{textAlign: 'center'}}>
                 <img style={imageStyle} src="https://img.icons8.com/color/512/000000/resume.png"></img>
                 <h5>Resume.pdf</h5>
               </a>
