ae9c54de1ab9894fe2b383aa85be0e4efc4b59bf
fadilnatakusumah <fadil.ntksmh@gmail.com>
2019-02-01 14:13:29 +0700

    build the web

---
+++ docs/precache-manifest.5b44ff21a555634f72f556f0f89443e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a0dafb5d8a1713943338e44a77e5584",
+    "url": "/static/media/preview.8a0dafb5.jpg"
+  },
+  {
+    "revision": "7fb4816a846577183a2f47e2cbc4b295",
+    "url": "/static/media/facedetection.7fb4816a.png"
+  },
+  {
+    "revision": "02f464b5e26d9479978370d9731754bc",
+    "url": "/static/media/bukuceritamu.02f464b5.PNG"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de48b8db5651870fad32",
+    "url": "/static/js/main.de48b8db.chunk.js"
+  },
+  {
+    "revision": "f70ddcd4f806804e32ef",
+    "url": "/static/js/1.f70ddcd4.chunk.js"
+  },
+  {
+    "revision": "de48b8db5651870fad32",
+    "url": "/static/css/main.f4338f11.chunk.css"
+  },
+  {
+    "revision": "cc7d8bba7434eff0f5a1a76237ac5ae7",
+    "url": "/index.html"
+  }
+];
