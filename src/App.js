79d436809ae154d217bb289fedd7d1ec5f124b55
fadilnatakusumah <fadil.ntksmh@gmail.com>
2019-04-05 17:40:31 +0800

    maintenance page

--- src/App.js
+++ src/App.js
@@ -10,19 +10,47 @@ import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
 
+import coffee from './assets/images/coffee.gif';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navigation />
-        <div className={'content'}>
-          <div className={'container'}>
-            <Navigation2 />
-            <About logo={logo}/>
-            <Portfolio logo={logo}/>
-            <Resume/>
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+      >
+        <div
+          style={{
+            backgroundImage: `url(${coffee})`,
+            backgroundSize: 'cover',
+            height: '100%',
+          }}
+        >
+
+          <div style={{
+            padding: '20px',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0.5)',
+            textAlign: 'center'
+          }}>
+            <h1 style={{ color: 'white' }}>Hey! Sorry, this website is still on maintenance</h1>
+            <h6 style={{
+              color: 'white',
+              position: 'fixed',
+              bottom: '30px',
+              textAlign: 'center',
+            }}>fadilnatakusumah</h6>
           </div>
-          <Footer/>
+
+
         </div>
       </div>
     );
