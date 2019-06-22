74d7681bdc300c697e37f25a33511859c78d1b48
fadilnatakusumah <fadil.ntksmh@gmail.com>
2019-06-22 22:10:55 +0700

    change header content

--- src/containers/Header/Content/Content.js
+++ src/containers/Header/Content/Content.js
@@ -4,7 +4,7 @@ import './Content.css'
 
 const content = () => (
     <div className="content-container">
-        <p className="large-title animated flipInY">Self-taught Web Developer</p>
+        <p className="large-title animated flipInY">Software Engineer</p>
         <p className="little-title animated flipInX">Always eager to learn and build apps with cutting edge technologies</p>
     </div>
 )
