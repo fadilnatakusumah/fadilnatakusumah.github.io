a8248a4f1a03fc08c9a78ec17ae8ebaca6459e80
fadilnatakusumah <fadil.ntksmh@gmail.com>
2019-09-12 19:55:32 +0700

    add new project Your Thoughts

--- src/data-env.js
+++ src/data-env.js
@@ -2,21 +2,9 @@
 import SSBukuCeritamu from './assets/gifs/snapshot_bukuceritamu.gif';
 import SSRandomCatsPicts from './assets/gifs/snapshot_randomcatpicts.gif';
 import SSJobFinder from './assets/gifs/snapshot_jobfinder.gif';
+import SSYourThoughts from './assets/gifs/snapshot_yourthoughts.gif';
 
 const dataProjects = [
-    {
-        projectName: 'BukuCeritamu',
-        description: 'Basicly a web based app that allows you to make a storybook with supportive pictures. Plus you can export it to PDF. (BukuCeritamu v2 still on progress)',
-        technologies: [
-            'Laravel (PHP)',
-            'FabricJS',
-            'JQuery',
-            'MySQL'
-        ],
-        linkGithub: 'https://github.com/fadilnatakusumah/bukuceritamu',
-        linkApp: 'https://fadilnatakusumah.000webhostapp.com/',
-        imageSrc: SSBukuCeritamu,
-    },
     // {
     //     projectName: 'Random Cat Pictures',
     //     description: 'Generate random cat pictures that got from API request',
@@ -28,6 +16,20 @@ const dataProjects = [
     //     linkApp: 'https://fadilnatakusumah.github.io/react-randomcat-api/',
     //     imageSrc: SSRandomCatsPicts,
     // },
+    {
+        projectName: 'Your Thoughts',
+        description: 'Social Media for sharing your thoughts, and let other people comment about that. (still not finish though)',
+        technologies: [
+            'React',
+            'Redux',
+            'React Router',
+            'Firebase Functions',
+            'Express',
+        ],
+        linkGithub: 'https://github.com/fadilnatakusumah/your-thoughts-client',
+        linkApp: 'https://your-thoughts-project.firebaseapp.com',
+        imageSrc: SSYourThoughts,
+    },
     {
         projectName: 'Job Finder',
         description: 'An App that provides jobs information at your area, and you can save the job first or directly apply to it',
@@ -42,6 +44,19 @@ const dataProjects = [
         linkGithub: 'https://github.com/fadilnatakusumah/react-native-job-finder',
         linkApp: 'https://expo.io/@fadilnatakusumah/jobFinder',
         imageSrc: SSJobFinder,
+    },
+    {
+        projectName: 'BukuCeritamu',
+        description: 'Basicly a web based app that allows you to make a storybook with supportive pictures. Plus you can export it to PDF. (BukuCeritamu v2 still on progress)',
+        technologies: [
+            'Laravel (PHP)',
+            'FabricJS',
+            'JQuery',
+            'MySQL'
+        ],
+        linkGithub: 'https://github.com/fadilnatakusumah/bukuceritamu',
+        linkApp: 'https://fadilnatakusumah.000webhostapp.com/',
+        imageSrc: SSBukuCeritamu,
     },
 ]
 
