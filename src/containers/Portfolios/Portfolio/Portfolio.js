b03154ac3ba5f8479d7b78147b93be4b4ac1a164
fadilnatakusumah <fadil.ntksmh@gmail.com>
2020-01-14 22:38:45 +0700

    update project view

--- src/containers/Portfolios/Portfolio/Portfolio.js
+++ src/containers/Portfolios/Portfolio/Portfolio.js
@@ -2,37 +2,39 @@ import React from 'react';
 
 import Label from '../../../components/Label/Label';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import TrackVisibility from 'react-on-screen';
 
 import './Portfolio.css';
 import 'animate.css';
 
-const portfolio = (props) => {
-    const { dataApp } = props;
-    if (props.isVisible) {
-        return (
-            <div className="portfolio-container animated zoomInLeft">
-                <LazyLoadImage
-                    alt={dataApp.projectName}
-                    src={dataApp.imageSrc? dataApp.imageSrc : "https://developersushant.files.wordpress.com/2015/02/no-image-available.png"}
-                    className={"app-preview"}
-                />
-                <div className={"app-description"}>
-                    <h1 >{dataApp.projectName}</h1>
-                    <p>{dataApp.description}</p>
-                    <h6>Technologies:</h6>
-                    <div className={"label-wrapper"}>
-                        {dataApp.technologies.map((tech, i) => (
-                            <Label title={tech} key={i} />
-                        ))}
+import dataApps from '../../../data-env';
+
+const portfolio = () => {
+    return (
+        <div className="portfolio-container animated zoomInLeft">
+            {dataApps.map(dataApp => (
+                <div className="app-wrapper">
+                    <div className="image-wrapper">
+                        <img src={dataApp.imageSrc ? dataApp.imageSrc : "https://developersushant.files.wordpress.com/2015/02/no-image-available.png"} />
                     </div>
-                    <div className={"button-app-wrapper"}>
-                        {dataApp.linkGithub && <a href={dataApp.linkGithub} rel="noopener noreferrer" target="_blank" >Github</a>}
-                        {dataApp.linkApp && <a href={dataApp.linkApp} rel="noopener noreferrer" target="_blank" >View App</a>}
+                    <div className="app-description">
+                        <h3>{dataApp.projectName}</h3>
+                        <p>{dataApp.description}</p>
+                        <div className="label-wrapper">
+                            {dataApp.technologies.map((tech, i) => (
+                                <label key={i}>{tech}</label>
+                            ))}
+                        </div>
+                        <div className="button-app-wrapper">
+                            {dataApp.linkGithub && <a href={dataApp.linkGithub} rel="noopener noreferrer" target="_blank" >Github</a>}
+                            {dataApp.linkApp && <a href={dataApp.linkApp} rel="noopener noreferrer" target="_blank" >View App</a>}
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
-    }
+            ))}
+
+        </div>
+    )
     return null;
 }
 
--- src/containers/Portfolios/Portfolios.js
+++ src/containers/Portfolios/Portfolios.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import TrackVisibility from 'react-on-screen';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
 import Portfolio from './Portfolio/Portfolio';
 import './Portfolios.css';
 
-import dataApps from '../../data-env';
 
 const portfolios = () => (
     <ScrollableAnchor id={"portfolios"}>
         <div className="portfolios-container">
             <h1 style={{ fontSize: '4em', color: 'purple', textAlign: 'center' }}>Latest work</h1>
-            {dataApps.map((data, i) => {
-                return (
-                    <TrackVisibility key={i} once>
-                        <Portfolio dataApp={data} />
-                    </TrackVisibility>
-                )
-            }).reverse()}
+            <Portfolio />
             <div className="viewMoreProjects">
                 <a href="https://github.com/fadilnatakusumah" target="_blank" rel="noopener noreferrer" title="GITHUB | fadilnatakusumah">
                     View more Projects
-            </a>
+                </a>
             </div>
         </div>
     </ScrollableAnchor>
