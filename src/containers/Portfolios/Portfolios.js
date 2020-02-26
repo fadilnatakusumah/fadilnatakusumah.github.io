82a39a7e9083a8bc26457cf19f1a442e499eacb7
fadilnatakusumah <fadil.ntksmh@gmail.com>
2020-02-26 09:02:01 +0700

    update projects

--- src/containers/Portfolios/Portfolios.js
+++ src/containers/Portfolios/Portfolios.js
@@ -8,7 +8,7 @@ import './Portfolios.css';
 const portfolios = () => (
     <ScrollableAnchor id={"portfolios"}>
         <div className="portfolios-container">
-            <h1 style={{ fontSize: '4em', color: 'purple', textAlign: 'center' }}>Latest work</h1>
+            <h1 style={{ fontSize: '4em', color: 'purple', textAlign: 'center' }}>Latest Dummy Projects</h1>
             <Portfolio />
             <div className="viewMoreProjects">
                 <a href="https://github.com/fadilnatakusumah" target="_blank" rel="noopener noreferrer" title="GITHUB | fadilnatakusumah">
