2909ee3a495fba727c5243e669e4004aa305c7ca
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-02-04 18:35:30 +0700

    under development

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import {
   useLocation,
 } from 'react-router-dom';
 import './App.scss';
-import { AnimatePresence} from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 
 import Profile from './containers/Profile';
 import Header from './components/Header';
+import { FaDownload, FaGithub } from 'react-icons/fa';
 const Projects = React.lazy(() => import('./containers/Projects'));
 const Contacts = React.lazy(() => import('./containers/Contacts'));
 
@@ -17,7 +18,21 @@ const App = () => {
   const location = useLocation()
   return (
     <div className="wrapperApp">
-      <Header />
+      <div>
+        <h2>Hi, sorry this web page is still under development :)</h2>
+        {/* <h4>Here check out my Github</h4> */}
+        <div>
+          <a target="_blank" rel="noreferrer" href="https://github.com/fadilnatakusumah">
+            <FaGithub />
+            <span>Github</span>
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/12y92QCDx7rsL7droLik6qJXHI3_iVW94/view?usp=sharing">
+            <FaDownload />
+            <span>Download Resume</span>
+          </a>
+        </div>
+      </div>
+      {/* <Header />
       <AnimatePresence exitBeforeEnter>
         <Switch key={location.pathname} location={location}>
           <Route path="/" exact >
@@ -36,7 +51,7 @@ const App = () => {
             </Suspense>
           </Route>
         </Switch>
-      </AnimatePresence>
+      </AnimatePresence> */}
     </div>
   );
 }
