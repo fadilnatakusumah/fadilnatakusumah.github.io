79e24d057585caa46deef52e2366da524d34783e
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-02-14 20:38:50 +0700

    adding package and configs

---
+++ config.ts
@@ -0,0 +1,7 @@
+import getConfig from "next/config"
+
+const { publicRuntimeConfig } = getConfig();
+
+export const APP_NAME = publicRuntimeConfig.APP_NAME;
+export const DOMAIN = "https://fadilnatakusumah.github.io";
+export const RESUME = "https://drive.google.com/file/d/12y92QCDx7rsL7droLik6qJXHI3_iVW94/view?usp=sharing";
---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: "Portfolio | Muhammad Fadhilah Mulyana",
+  },
+  // compilerOptions: {
+  //   "baseUrl": ".",
+  // },
+  crossOrigin: 'anonymous',
+}
