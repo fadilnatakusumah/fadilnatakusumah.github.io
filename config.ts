73447e63534a11c013c0d324656d35735d4bd552
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-02-14 23:15:22 +0700

    update resume

--- config.ts
+++ config.ts
@@ -4,4 +4,4 @@ const { publicRuntimeConfig } = getConfig();
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 export const DOMAIN = "https://fadilnatakusumah.github.io";
-export const RESUME = "https://drive.google.com/file/d/12y92QCDx7rsL7droLik6qJXHI3_iVW94/view?usp=sharing";
+export const RESUME = "https://drive.google.com/file/d/1-aXOxJntdvTLO4azqjttsSLTCbINRuZG/view";
