2b0c4cadfff384a4a488d3bc8644bcca8b484eb2
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-02-15 09:43:02 +0700

    fix meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -48,17 +48,17 @@ export function Layout({
       <Head>
         <title>{title || "Portfolio"} - Muhammad Fadhilah Mulyana</title>
         <meta name='description' content={description || 'Porfolio website for Muhammad Fadhilah Mulyana'} />
-        <link rel="canonical" href={`${DOMAIN}/${router.pathname}`} />
+        <link rel="canonical" href={`${DOMAIN}${router.pathname}`} />
         <meta property="og:title" content={title} />
         <meta name="og:description" content={description} />
         <meta name="og:type" content="website" />
-        <meta name="og:url" content={`${DOMAIN}/${router.pathname}`} />
+        <meta name="og:url" content={`${DOMAIN}${router.pathname}`} />
         <meta name="og:site_name" content={`${APP_NAME}`} />
 
-        <meta name="og:image" content={`${DOMAIN}/assets/images/profile-photo.jpg`} />
+        <meta name="og:image" itemProp="image" content={`${DOMAIN}/assets/images/profile-photo.jpg`} />
         <meta name="og:image:secure_url" content={`${DOMAIN}/assets/images/profile-photo.jpg`} />
         <meta name="og:image:type" content={`image/jpg`} />
-        <meta name="fb:350804358856723" content={`${APP_NAME}`} />
+        {/* <meta name="fb:350804358856723" content={`${APP_NAME}`} /> */}
       </Head>
       <div>
         <Header pageId={pageId} />
