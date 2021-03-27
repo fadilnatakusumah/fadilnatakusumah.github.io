308f6d46879b6eb51495dc7373a525a0371fcde3
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-03-27 22:38:43 +0700

    update resume link

--- config.ts
+++ config.ts
@@ -4,4 +4,4 @@ const { publicRuntimeConfig } = getConfig();
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 export const DOMAIN = "https://fadilnatakusumah.vercel.app";
-export const RESUME = "https://drive.google.com/file/d/15DkmPgindsdue1pK7HtNUHMU8CI4JVxg/view?usp=sharing";
+export const RESUME = "https://drive.google.com/file/d/1J9UG_4eREd0BNQClEg3yHzOF1d2wFYAN/view";
