c5399f5a015546d679fdabc71f02d2c83cae20a2
fadilnatakusumah <fadil.ntksmh@gmail.com>
2024-04-25 02:54:28 +0700

    feat: update resume linik

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ export const DOMAIN =
     ? "https://fadilnatakusumah.vercel.app"
     : "";
 export const RESUME =
-  "https://drive.google.com/file/d/18mZCB0uBRfJKIVPe54oROR2bNEywPwpP/view";
+  "https://drive.google.com/file/d/1vv4HdNaz2SEEiwIGiPlzoEAx2fzQRl-k/view?usp=sharing";
 export const HACKERRANK = "https://www.hackerrank.com/fadil_ntksmh";
 export const GITHUB = "https://github.com/fadilnatakusumah";
 export const LINKEDIN = "https://linkedin.com/in/muhammad-fadhilah-mulyana";
