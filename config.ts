d6e463c24c951a3b47ac02444a811d3de4c18da6
Fadil Natakusumah <fadil.ntksmh@gmail.com>
2024-07-06 16:12:34 +0700

    Update RESUME

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ export const DOMAIN =
     ? "https://fadilnatakusumah.vercel.app"
     : "";
 export const RESUME =
-  "https://drive.google.com/file/d/18mZCB0uBRfJKIVPe54oROR2bNEywPwpP/view?usp=drivesdk";
+  "https://drive.google.com/file/d/1L4rzoe6BqQ83LBWtPp1ktpjsygkDZYiI/view?usp=sharing";
 export const HACKERRANK = "https://www.hackerrank.com/fadil_ntksmh";
 export const GITHUB = "https://github.com/fadilnatakusumah";
 export const LINKEDIN = "https://linkedin.com/in/muhammad-fadhilah-mulyana";
